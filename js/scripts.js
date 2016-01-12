ff6ba5d28746e9ab88372e19534b88f317f272eb
jared beckler and torrence stratton <student@epicodus.com>
2016-01-12 09:42:27 -0800

    Add img with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+  $("#catimg").hide();
+
   $("button#cat").click(function(){
-    $("ul").prepend("<li>meow</li>");
-    $("li").css('background-color', 'green');
+    $("ul#user").prepend("<li>I'm a kitty! meow!</li>");
+    $("ul#webpage").prepend("<li>You're silly.</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $("#catimg").show();
+    });
+  });
+
+  $("#catimg").click(function(){
+    $("#catimg").hide();
   });
 
   $("button#dog").click(function(){
-    $("ul").prepend("<li>WOOF</li>");
+    $("ul#user").prepend("<li>Imma dog! WOOF!</li>");
+    $("ul#webpage").prepend("<li>Stop kidding yourself...</li>");
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
